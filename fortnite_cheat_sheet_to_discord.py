0a1fbdf68ff1d62c525ee9719e56718313c8d10e
Isaac K <ethernic@voidwall.com>
2019-02-02 18:17:32 -0700

    Rev 2

--- fortnite_cheat_sheet_to_discord.py
+++ fortnite_cheat_sheet_to_discord.py
@@ -1,39 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 import os.path
 
-discordWebhook = ''  # Enter your discord webhook.
+discordWebhook = "" # Enter your discord webhook.
 filename = 'links_posted.txt'
+
+if os.path.isfile('config.json'):
+    try:
+        with open('config.json', 'r') as conf:
+            config = json.loads(conf.read)
+        discordWebhook = config['webhook']
+    except:
+        quit()
 
 # Check and see if file has been created. If not, create it.
 if os.path.isfile(filename):
     # Read the lines.
-    f = open(filename, 'r')
-    lines = f.read()
+    with open(filename, 'r') as f:
+        lines = f.read()
 else:
-    f = open(filename, 'x')
-    lines = []
+    with open(filename, 'x') as f:
+        lines = []
 
 # Get all stories from fortniteinsider.
 r = requests.get(
     'https://fortniteinsider.com/?s=cheat')
 parsed_html = BeautifulSoup(r.text, 'html.parser')
-
 h3s = parsed_html.findAll('h3', {'class': 'entry-title'})
-
 for h3 in h3s:
     if 'cheat' in h3.text.lower():
         for a in h3.contents:
             link = a.attrs['href']
-            if link not in lines:
-                # Post to discord.
+            cheat_sheet = requests.get(link)
+            parsed_cheat = BeautifulSoup(cheat_sheet.text, 'html.parser')
+            imgs = parsed_cheat.findAll('img', {'class': 'entry-thumb'})
+            for img in imgs:
+                if "Fortnite-Cheat-Sheet-Map" in img['src']:
+                    cheat_link = img['src']
+            if cheat_link not in lines:
+
                 data = {
-                    "username": "Webhook",
-                    "avatar_url": "https://i.imgur.com/4M34hi2.png",
-                    "content": link
+                    "username": "Etherboten",
+                    "avatar_url": "https://i.imgur.com/TeMahcP.png",
                 }
-                discord = requests.post(discordWebhook, data=data)
+                # Get the image
+                file = requests.get(cheat_link).content
+                headers = {}
+                payload = {'payload_json': json.dumps(data)}
+                multipart = {'file': ('cheat_sheet.png', file)}
+
+                # Post to discord.
+                discord = requests.post(discordWebhook, data=payload, headers=headers, files=multipart)
 
                 # Write the link to the file to not post again.
-                f = open(filename, 'a')
-                f.write(link + '\n')
+                with open(filename, 'a') as f:
+                    f.write(cheat_link + '\n')
